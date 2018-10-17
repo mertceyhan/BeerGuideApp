90b2af93f3dbd471cabe6d4f5830926f1cf0b154
Cafer Mert Ceyhan <c.mertceyhan@hotmail.com>
2018-10-17 15:22:36 +0300

    gitignore updated

---
+++ app/src/main/java/com/mertceyhan/beerguideapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mertceyhan.beerguideapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
